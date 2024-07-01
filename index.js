59109a68f4eefcb56ac797d4e630a4156ead4c1a
Darshan Bulsara <dbulsara81@gmail.com>
2024-06-30 20:48:38 -0500

    added sudoku board

--- index.js
+++ index.js
@@ -3,7 +3,30 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
 import {name as appName} from './app.json';
+import React from 'react';
+import { View, StyleSheet} from 'react-native';
+import SudokuBoard from './components/sudokuBoard';
+
 
 AppRegistry.registerComponent(appName, () => App);
+
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <SudokuBoard/>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+});
+
+export default App;
